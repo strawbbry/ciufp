daca0f88a7717fc76679a3124033b1bf277d35ac
strawbbry <anushakundargi@gmail.com>
2026-06-17 21:49:06 -0400

    FP Basic Data Types

---
+++ fp.py
@@ -0,0 +1,43 @@
+# basic data types 
+
+lst = []
+range(3) = [0, 1, 2]
+
+"name"[0:3] = "name"[:3] # = "nam" 
+"name"[3:] = "e"
+"name"[3:-1] = ""
+
+# sorting : O(nlogn)
+# always try to sort first ! 
+sorted_lst = sorted(lst, key=None, reverse=False) # new array
+lst.sort(key=None, reverse=False)  # mutate array 
+
+# key = rule to sort by
+# lambda input: output 
+lst = [(1, 'a'), (2, 'b')]
+sorted_lst = sorted(lst, key=lambda x: x[0]) 
+lst.sort(key=lambda x: x[1]) 
+
+
+dict = {}
+dict['a'] = 1 # {'a' : 1}
+# for key, value in dict
+
+from collections import defaultdict
+dict = defaultdict(int) # default 0
+dict = defaultdict(list) # default []
+
+
+# element in set : O(1)
+# faster than element in list : O(n) !
+set = set([1], 2) # unordered data 
+set.add('3')
+set.remove(2)
+
+
+str = "hi u"
+indiv = str.split() # ["hi", "u"] 
+" ".join(indiv) = "hi u" # join with " "
+str.replace(" ", "") = "hiu"
+
+
