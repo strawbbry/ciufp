150eb58d16b7be0af4289f355b61627599d6abf4
strawbbry <ark279@cornell.edu>
2026-06-19 15:12:09 -0400

    CIU Linked Lists

--- ciu.cpp
+++ ciu.cpp
@@ -98,3 +98,417 @@ class Vector {
             return -1;
         }
 };
+
+#include <string>
+// singly linked list: track prev = curr via curr->next  
+// doubly linked list: prev pointer 
+class LinkedList {
+    protected:
+        struct node {
+            std::string data;
+            node *prev; 
+            node *next;
+        };
+
+        node *head;
+        // add int size to make size() O(1)
+    
+    public:
+        LinkedList (std::string data = "") { // constructor 
+            if (data == "") { // empty node = no node 
+                head = nullptr;
+            } else {
+                head = new node {data, nullptr, nullptr}; // 'new' = c 'malloc'
+            }
+        }
+
+        // virtual : look up parent vs child def at runtime ! 
+        // don't just auto use parent def even for child
+        virtual ~LinkedList () { 
+            node *curr = head; // iterate thru linkedlist
+            while (curr != nullptr) {
+                node *next = curr->next; // -> bc curr is ptr 
+                delete curr; // 'delete' = c 'free'
+                curr = next; 
+            }
+            head = nullptr;
+        }
+
+        int size () {
+            int i = 0;
+            node *curr = head;
+            while (curr != nullptr) {
+                node *next = curr->next;
+                i++;
+                curr = next;
+            }
+            return i;
+        }
+
+        bool empty () {
+            if (head == nullptr) {
+                return true;
+            }
+            return false;
+        }
+
+        virtual std::string value_at (int n) {
+            if (n < 0 || n > size() - 1) { return ""; }
+            int i = 0;
+            node *curr = head;
+            while (curr != nullptr) {
+                if (i == n) {
+                    return curr->data;
+                } else {
+                    node *next = curr->next;
+                    i++;
+                    curr = next;
+                }
+            }
+            return "";
+        }
+
+        void push_front (std::string value) {
+            node *push = new node {value, nullptr, head};
+            if (head != nullptr) {
+                head->prev = push;
+            }
+            head = push;
+        }
+
+        std::string pop_front () {
+            if (head == nullptr) { return ""; }
+
+            std::string value = head->data;
+
+            node *pop = head;
+            head = head->next;
+            if (head != nullptr) {
+                head->prev = nullptr;
+            }
+            delete pop; // need to actl free the allocated heap of node !
+            
+            return value;
+        }
+
+        virtual void push_back (std::string value) {
+            node *push = new node {value, nullptr, nullptr};
+
+            if (head == nullptr) { // 1. list = empty
+                head = push;
+            } else {
+                node *curr = head;
+                while (curr->next != nullptr) { // iterate until last node
+                    curr = curr->next;
+                }
+                curr->next = push;
+                push->prev = curr;
+            }
+        }
+
+        virtual std::string pop_back () {
+            if (head == nullptr) { return ""; } // 1. 0 node
+
+            if (head->next == nullptr) { // 2. only 1 node 
+                std::string value = head->data;
+                delete head; // 
+                head = nullptr;
+                return value;
+            } else { // 3. >1 node
+                node *curr = head;
+                while (curr->next != nullptr) {
+                    curr = curr->next;
+                }
+                std::string value = curr->data;
+                curr->prev->next = nullptr;
+                delete curr;
+                return value;
+            }
+        }
+
+        std::string front () {
+            return head->data;
+        }
+
+        virtual std::string back () {
+            if (head == nullptr) { return ""; }
+            else {
+                node *curr = head;
+                while (curr->next != nullptr) {
+                    curr = curr->next;
+                }
+                return curr->data;
+            }
+        }
+
+        virtual void insert (int index, std::string value) {
+            if (size() < index + 1) { return; } // index too big
+
+            node *in = new node {value, nullptr, nullptr};
+
+            if (index == 0) { // index at start
+                push_front(value);
+                return;
+            }
+            if (index == size()) { // index at end + 1
+                push_back(value);
+                return;
+            }
+            else { // index in middle 
+                int i = 0;
+                node *curr = head;
+                while (curr->next != nullptr) {
+                    if (i == index) {
+                        in->prev = curr->prev;
+                        curr->prev->next = in;
+                        in->next = curr;
+                        curr->prev = in; 
+                        return;
+                    }
+                    curr = curr->next;
+                    i++;
+                }
+            }
+        }
+
+        virtual void erase (int index) {
+            if (size() < index + 1) { return; } // index too big
+            if (index == 0) { // index at start
+                std::string x = pop_front();
+                return;
+            }
+            if (index == size() - 1) { // index at end
+                std::string x = pop_back();
+                return;
+            }
+            else {
+                node *curr = head;
+                for (int i = 0; i < index; i++) {
+                    curr = curr->next;
+                }
+                curr->prev->next = curr->next;
+                curr->next->prev = curr->prev;
+                delete curr;
+            }
+
+        }
+
+        virtual std::string value_n_from_end (int n) {      
+            return value_at(size() - n);
+        }
+
+        virtual void reverse () {
+            if (head == nullptr || head->next == nullptr) { return; }
+            node *curr = head;
+            node *temp = nullptr;
+            while (curr != nullptr) {
+                temp = curr->prev;
+                curr->prev = curr->next;
+                curr->next = temp;
+
+                curr = curr->prev; // move forward 
+            }
+            head = temp->prev;
+        }
+
+        virtual void remove_value (std::string value) {
+            if (head == nullptr) { return; }
+            if (head->data == value) {
+                std::string x = pop_front();
+                return;
+            }
+            node *curr = head;
+            while (curr != nullptr) {
+                if (curr->data == value) {
+                    curr->prev->next = curr->next;
+                    if (curr->next != nullptr) {
+                        curr->next->prev = curr->prev;
+                    }
+                    delete curr;
+                    return;
+                }
+                curr = curr->next;
+            }
+            return;
+        }
+
+};
+
+class LinkedList_TailPointer : public LinkedList {
+    protected:
+        node *tail;
+    
+    public:
+        LinkedList_TailPointer (std::string data = "") {
+            if (data == "") {
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                head = new node {data, nullptr, nullptr};
+                tail = head; 
+            }
+        };
+
+        ~LinkedList_TailPointer () {
+            // compiler auto calls ~LinkedList !
+            tail = nullptr;
+        }
+
+        std::string value_at (int n) override {
+            if (n < 0 || n > size() - 1) { return ""; }
+            node *curr;
+            if (n < size() / 2) {
+                curr = head;
+                for (int i = 0; i < n; i++) {
+                    curr = curr->next;
+                }
+            } else {
+                curr = tail;
+                for (int i = size() - 1; i > n; i--) {
+                    curr = curr->prev;
+                }
+            }
+            return curr->data;
+        }
+
+        void push_back (std::string value) override {
+            node *push = new node {value, nullptr, nullptr};
+            if (head == nullptr) { // 1. empty list
+                head = push;
+                tail = push;
+            } 
+            else { // 2. nonempty list
+                tail->next = push;
+                push->prev = tail;
+                tail = push; 
+            }
+        }
+
+        std::string pop_back () override {
+            if (head == nullptr) { return ""; } // 0 node 
+            std::string value = tail->data;
+
+            if (head == tail) { // 1 node
+                delete head;
+                head = nullptr;
+                tail = nullptr;
+                return value;
+            } else { // >1 node
+                node *pop = tail;
+                tail = tail->prev;
+                tail->next = nullptr;
+                delete pop;
+                return value;
+            }
+        }
+
+        std::string back () override {
+            if (head == nullptr) { return ""; }
+            return tail->data;
+        }
+
+        void insert (int index, std::string value) override {
+            if (size() < index + 1) { return; } // index too big
+
+            node *in = new node {value, nullptr, nullptr};
+
+            if (index == 0) { // index at start
+                push_front(value);
+                return;
+            }
+            if (index == size()) { // index at end + 1
+                push_back(value);
+                return;
+            }
+            else {
+                node *curr;
+                if (index < size() / 2) { // index < midpoint
+                    curr = head;
+                    for (int i = 0; i < index; i++) {
+                        curr = curr->next;
+                    }
+                } else { // index >= midpoint
+                    curr = tail;
+                    for (int i = size(); i > index; i--) {
+                        curr = curr->prev;
+                    }
+                }
+                in->prev = curr->prev;
+                curr->prev->next = in;
+                in->next = curr;
+                curr->prev = in;  
+            }
+        }
+
+        void erase (int index) override {
+            if (size() < index + 1) { return; } // index too big
+            if (index == 0) { // index at start
+                pop_front();
+                return;
+            }
+            if (index == size()) { // index at end + 1
+                pop_back();
+                return;
+            }
+            else {
+                node *curr;
+                if (index < size() / 2) {
+                    curr = head;
+                    for (int i = 0; i < index; i++) {
+                        curr = curr->next;
+                    }
+                } else {
+                    curr = tail;
+                    for (int i = size() - 1; i > index; i--) {
+                        curr = curr->prev;
+                    }
+                }
+                curr->prev->next = curr->next;
+                curr->next->prev = curr->prev;
+                delete curr;
+            }
+        }
+
+        std::string value_n_from_end (int n) override {
+            return value_at(size() - n); // override to use child fn value_at
+        }
+
+        void reverse () override {
+            if (head == nullptr || head->next == nullptr) { return; }
+            node *curr = head;
+            node *temp = nullptr;
+            while (curr != nullptr) {
+                temp = curr->prev;
+                curr->prev = curr->next;
+                curr->next = temp;
+
+                curr = curr->prev; // move forward 
+            }
+            node *old = head;
+            head = tail;
+            tail = old;
+        }
+
+        void remove_value (std::string value) override {
+            if (head == nullptr) { return; }
+            if (head->data == value) {
+                std::string x = pop_front();
+                return;
+            }
+            if (tail->data == value) {
+                std::string x = pop_back();
+                return;
+            }
+            node *curr = head;
+            while (curr != nullptr) {
+                if (curr->data == value) {
+                    curr->prev->next = curr->next;
+                    curr->next->prev = curr->prev;
+                    delete curr;
+                    return;
+                }
+                curr = curr->next;
+            }
+            return;
+        }
+};  
